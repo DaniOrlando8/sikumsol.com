268ed366066c7b55be3dfd661dde51ce61e1a669
DaniOrlando8 <daniorlando8@gmail.com>
2022-05-26 18:30:28 +0700

    26/05/2022

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0093AD",
+        secondary: "#64748b",
+        dark: "#1F2855",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
